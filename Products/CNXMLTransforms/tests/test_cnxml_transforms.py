b0c0dd26962cd30116b85d05890bb5b91c88d446
Ross J. Reedstrom <reedstrm@cnx.org>
2012-01-18 23:00:05 +0000

    merge oerpub-milestone4

--- Products/CNXMLTransforms/tests/test_cnxml_transforms.py
+++ Products/CNXMLTransforms/tests/test_cnxml_transforms.py
@@ -23,7 +23,8 @@
 $Id: $
 """
 
-
+import os
+import difflib
 from Products.RhaptosTest import config
 import Products.CNXMLTransforms
 config.products_to_load_zcml = [('configure.zcml', Products.CNXMLTransforms),]
@@ -31,6 +32,19 @@ config.products_to_install = ['CNXMLTransforms']
 
 from Products.RhaptosTest import base
 
+from Products.CNXMLTransforms.helpers import doTransform
+
+from Testing import ZopeTestCase
+ZopeTestCase.installProduct('RhaptosSword')
+ZopeTestCase.installProduct('CNXML')
+ZopeTestCase.installProduct('UniFile')
+
+DIRNAME = os.path.dirname(__file__)
+
+def diff(a, b):
+    return '\n'.join(
+        difflib.unified_diff(a.splitlines(), b.splitlines())
+        )
 
 class TestCNXMLTransforms(base.RhaptosTestCase):
 
@@ -51,6 +65,88 @@ class TestCNXMLTransforms(base.RhaptosTestCase):
 
     def test_zip_to_folder(self):
         self.assertEqual(1, 1)
+
+    def test_sword_to_folder(self):
+        context = self.folder
+        name = "sword_to_folder"
+        filename = 'module.zip'
+        path = os.path.join(DIRNAME, 'data', filename)
+        file = open(path, 'rb')
+        data = file.read()
+        file.close()
+        meta = 1
+        kwargs = {}
+
+        text, subobjs, meta = doTransform(
+            context, name, data, meta=1, **kwargs)
+        
+        returned_text = text.read()
+        path = os.path.join(DIRNAME, 'data', 'no_links_reference.xml')
+        file = open(path, 'rb')
+        no_links_reference_text = file.read()
+        file.close()
+
+        self.assertEqual(
+            returned_text,
+            no_links_reference_text,
+            'The text was not extracted correctly. (%s)' % \
+                diff(returned_text, no_links_reference_text)
+        )
+
+        self.assertEqual(subobjs, {}, 'There should be no sub objects.')
+        self.assertEqual(
+            meta, 
+            {'mimetype': 'application/cmf+folderish',
+             'featured_links': [],
+             'properties': {},
+             'encoding': None},
+            'Metadata was not set correctly.')
+
+    def test_import_new_links(self):
+        context = self.folder
+        name = "sword_to_folder"
+        filename = 'module_with_one_featured_link.zip'
+        path = os.path.join(DIRNAME, 'data', filename)
+        file = open(path, 'rb')
+        data = file.read()
+        file.close()
+        meta = 1
+        kwargs = {}
+
+        text, subobjs, meta = doTransform(
+            context, name, data, meta=1, **kwargs)
+
+        path = os.path.join(DIRNAME, 'data', 'one_link_reference.xml')
+        file = open(path, 'rb')
+        one_link_reference_text = file.read()
+        file.close()
+
+        returned_text = text.read()
+        self.assertEqual(
+            returned_text,
+            one_link_reference_text,
+            'The text was not extracted correctly. (%s)' % \
+                diff(returned_text, one_link_reference_text)
+        )
+        self.assertEqual(subobjs, {}, 'There should be no sub objects.')
+        self.assertEqual(
+            meta,
+            {'mimetype': 'application/cmf+folderish',
+             'featured_links': [{'url': 'http://localhost:8080/featured_module',
+                                 'strength': '3',
+                                 'type': 'example',
+                                 'title': 'Test feature link'}
+                               ],
+             'properties': {},
+             'encoding': None
+            },
+            'Metadata was not set correctly.')
+
+    def test_import_existing_links(self):
+        self.fail()
+
+    def test_import_new_and_existing_links(self):
+        self.fail()
 
 
 def test_suite():
